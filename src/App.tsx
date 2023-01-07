0a8a01bf936d2f1bb7399e649e2a041e745c8f33
Iikku Mattila <iikku@iikku.org>
2023-01-07 19:10:38 +0200

    Add a sharpness parameter for mutating the sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import Sound from './Sound/Sound';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Sound />
+        Zädäng
       </header>
+      <Sound />
     </div>
   );
 }
--- src/Sound/Sound.tsx
+++ src/Sound/Sound.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 function Sound() {
+  const [inversePower, setInversePower] = useState(0);
 
   // Create an array to hold the oscillators
   const numberOfHarmonics = 30;
@@ -6,16 +9,19 @@ function Sound() {
   const volumes: GainNode[] = [];
   let audioCtx: AudioContext;
 
+  let connected = false;
+
   function connect() {
+    if (connected) return;
     // Create an audio context
     audioCtx = new (window.AudioContext)();
 
     // Create the oscillators
-    for (var i = 0; i < numberOfHarmonics; i++) {
+    for (var i = 1; i <= numberOfHarmonics; i++) {
       const oscillator = audioCtx.createOscillator();
       oscillators[i] = oscillator;
       oscillator.type = 'sine';
-      oscillator.frequency.value = 261.63 * (i + (Math.random() > 0.8 ? 0.5 : 0));
+      oscillator.frequency.value = 261.63 * i;
 
       const volume = audioCtx.createGain();
       volumes[i] = volume;
@@ -27,18 +33,26 @@ function Sound() {
     }
   }
 
+  // 0 -- 1
+  function startingGainForHarmonic(harmonic: number) {
+    return Math.pow(1 / harmonic, inversePower);
+
+//    const lowBiasedRandom = Math.pow(Math.random(), harmonic * 5);
+//    return lowBiasedRandom * (10 - 1/(harmonic + 1));
+  }
+
   function playSound() {
-    for (var i = 0; i < numberOfHarmonics; i++) {
+    connect();
+    for (var i = 1; i <= numberOfHarmonics; i++) {
       const gain = volumes[i].gain;
-      const lowBiasedRandom = Math.pow(Math.random(), i * 5);
-      gain.setValueAtTime(lowBiasedRandom * (10 - 1/(i+1)), audioCtx.currentTime);
+      gain.setValueAtTime(startingGainForHarmonic(i), audioCtx.currentTime);
       gain.linearRampToValueAtTime(0, audioCtx.currentTime + Math.random() * 2);
     }
   }
 
 
   function stopSound() {
-    for (var i = 0; i < numberOfHarmonics; i++) {
+    for (var i = 1; i <= numberOfHarmonics; i++) {
       const gain = volumes[i].gain;
       gain.setValueAtTime(0, audioCtx.currentTime);
     }
@@ -46,9 +60,10 @@ function Sound() {
 
   return (
     <div>
-      <button onClick={connect}>
-        connect
-      </button>
+      Sharpness:
+      <input type="number" step="0.1" value={inversePower} onChange={e => setInversePower(+e.target.value)} />
+
+      <br />
       <button onClick={playSound}>
         play
       </button>
